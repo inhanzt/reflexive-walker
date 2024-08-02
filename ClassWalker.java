3c0c50864ea1e949cf52f08274e0bcdfd454be73
inhanzt <inhanzt@gmail.com>
2024-08-02 09:23:52 -0400

    Add files via upload

---
+++ ClassWalker.java
@@ -0,0 +1,157 @@
+package uci.example;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+class UUIDHaver {
+    UUID myId;
+}
+
+class ThingHaver {
+    Object thing;
+}
+
+class ThingReturner extends ThingHaver {
+
+    public UUIDHaver getUUIDHaver() {
+        if (thing instanceof UUIDHaver) {
+            return (UUIDHaver) thing;
+        }
+        return null;
+    }
+}
+
+class GenericThingReturner extends ThingHaver {
+
+    public List<UUIDHaver> getUUIDHaver() {
+        List<UUIDHaver> list = new ArrayList<>();
+        if (thing instanceof UUIDHaver) {
+            list.add((UUIDHaver) thing);
+        }
+        return list;
+    }
+}
+
+public class ClassWalker {
+    public static void main(String [] args) throws Exception {
+        Class<?> clazz = HashMap.class;
+        System.out.println(clazz + ":");
+        System.out.println(canFindClassInClass(clazz, new HashSet<>(), UUID.class)); //false
+
+        clazz = UUIDHaver.class;
+        System.out.println(clazz + ":");
+        System.out.println(canFindClassInClass(clazz, new HashSet<>(), UUID.class)); //true
+
+        clazz = ThingHaver.class;
+        System.out.println(clazz + ":");
+        System.out.println(canFindClassInClass(clazz, new HashSet<>(), UUID.class)); //false. 
+        // Even if thingHaver's object can be a UUIDHaver, the class definition doesn't suggest a UUID is supported.
+
+        clazz = ThingReturner.class;
+        System.out.println(clazz + ":");
+        System.out.println(canFindClassInClass(clazz, new HashSet<>(), UUID.class)); //true
+
+        clazz = GenericThingReturner.class;
+        System.out.println(clazz + ":");       
+        System.out.println(canFindClassInClass(clazz, new HashSet<>(), UUID.class)); //true
+    }
+
+    /**
+     * Basically, just tell me if soughtClass CAN EVER exist in startingClass, recursively, anywhere in the class structure.
+     * 
+     * @param startingClass
+     * @param visited
+     * @param soughtClass
+     * @return
+     */
+    public static boolean canFindClassInClass(Class<?> startingClass, Set<Class<?>> visited, Class<?> soughtClass) {
+            
+        if(visited.contains(startingClass)) {
+            return false;
+        }
+        visited.add(startingClass);
+        
+
+        List<Field> allFields = getAllFields(startingClass, Object.class);
+        for(Field f : allFields) {
+            Class<?> type = f.getType();
+            if(soughtClass.isAssignableFrom(type)) {
+                return true;
+            }
+            boolean filter = canFindClassInClass(type, visited, soughtClass);
+            if (filter) {
+                return true;
+            }
+
+        }
+        Set<Class<?>> allFunctionReturns = getAllFunctionReturns(startingClass, Object.class);
+        for(Class<?> c : allFunctionReturns) {
+            if(soughtClass.isAssignableFrom(c)) {
+                return true;
+            }
+            boolean filter = canFindClassInClass(c, visited, soughtClass);
+            if (filter) {
+                return true;
+            } 
+        }
+
+        return false;
+    }
+
+    private static List<Field> getAllFields(Class<?> start, Class<?> exclusiveParent) {
+        
+        List<Field> allFields = new ArrayList<>(Arrays.asList(start.getDeclaredFields()));
+
+        Class<?> parent = start.getSuperclass();
+
+        if(parent != null &&
+            (exclusiveParent == null || !(parent.equals(exclusiveParent)))
+        ) {
+            List<Field> inheritedFields = getAllFields(parent, exclusiveParent);
+            allFields.addAll(inheritedFields);
+        }
+
+        return allFields;
+    }
+
+    private static Set<Class<?>> getAllFunctionReturns(Class<?> start, Class<?> exclusiveParent) {
+        
+        List<Method> allMethods = new ArrayList<>(Arrays.asList(start.getDeclaredMethods()));
+        Set<Class<?>> allFields = new HashSet<>();
+        for (Method method : allMethods) {
+            allFields.add(method.getReturnType());
+            Type genericType = method.getGenericReturnType();
+            if (genericType instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) genericType;
+                Type[] elementTypes = parameterizedType.getActualTypeArguments();
+                for(Type elementType : elementTypes) {
+                    if (elementType instanceof Class) {
+                        Class<?> elementType2 = (Class<?>)elementType;
+                        allFields.add(elementType2);
+                    }
+                }
+            }
+            
+        }
+
+        Class<?> parent = start.getSuperclass();
+
+        if(parent != null &&
+            (exclusiveParent == null || !(parent.equals(exclusiveParent)))
+        ) {
+            Set<Class<?>> inheritedFields = getAllFunctionReturns(parent, exclusiveParent);
+            allFields.addAll(inheritedFields);
+        }
+
+        return allFields;
+    }
+}
---
+++ ObjectWalker.java
@@ -0,0 +1,146 @@
+package uci.example;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+class UUIDHaver {
+    UUID myId;
+}
+
+class ThingHaver {
+    Object thing;
+}
+
+public class ObjectWalker {
+    public static void main(String [] args) throws Exception {
+
+        List<Object> myList = new ArrayList<>();
+        Set<Object> mySet = new HashSet<>();
+        Map<Object, Object> myMap = new HashMap<>();
+        UUIDHaver uuidHaver = new UUIDHaver();
+        uuidHaver.myId = UUID.randomUUID();
+
+        myMap.put("string", mySet);
+        mySet.add(myList);
+
+        Set<UUID> seeking = new HashSet<>();
+
+        // Not looking for anything, don't have anything.
+        boolean hasValueInObject = hasValueInObject(myMap, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // false
+
+        // Do have something, not looking for anything.
+        myList.add(uuidHaver);
+        hasValueInObject = hasValueInObject(myMap, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // false
+
+        // Do have something, looking for wrong thing.
+        seeking.add(UUID.randomUUID());
+        hasValueInObject = hasValueInObject(myMap, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // false
+
+        // Looking for thing, have that thing.
+        seeking.clear();
+        seeking.add(uuidHaver.myId);
+        hasValueInObject = hasValueInObject(myMap, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // true
+
+        // Looking for something, but don't have anything.
+        myList.clear();
+        hasValueInObject = hasValueInObject(myMap, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // false
+
+        // Looking for thing, have that thing. Outside of a collection.
+        ThingHaver thingHaver = new ThingHaver();
+        thingHaver.thing = uuidHaver;
+        hasValueInObject = hasValueInObject(thingHaver, seeking, UUIDHaver.class, "myId");
+        System.out.println(hasValueInObject); // true
+    }
+
+    private static boolean hasValueInObject(Object startingObject, Set<?> soughtValues, Class<?> soughtClass, String soughtFieldOfSoughtClass) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        
+        Set<Object> foundValues = new HashSet<>();
+        
+        findValuesInObject(startingObject, new HashSet<>(), foundValues, soughtClass, soughtFieldOfSoughtClass);
+        
+        for(Object o : foundValues) {
+            if(soughtValues.contains(o)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void findValuesInObject(Object o, Set<Object> visited, Set<Object> foundValues, Class<?> soughtClass, String soughtField) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        
+        if(o == null) {
+            return;
+        }
+
+        // If we found our thing; pull the data.
+        if(soughtClass.isAssignableFrom(o.getClass())) {
+            List<Field> allFields = getAllFields(soughtClass, Object.class);
+            for(Field f : allFields) {
+                if (soughtField.equals(f.getName())) {
+                    f.setAccessible(true);
+                    Object object = f.get(o);
+                    foundValues.add(object);
+                    return;
+                }
+            }
+           throw new NoSuchFieldException(soughtField);
+        }
+
+        // If we found an array, we need to inspect each element.
+        if(o.getClass().isArray()) {
+            int length = Array.getLength(o);
+            for(int i = 0; i < length; i++) {
+                Object object = Array.get(o, i);
+                if(visited.contains(object)) {
+                    continue;
+                }
+                visited.add(o);
+                findValuesInObject(object, visited, foundValues, soughtClass, soughtField);
+            }
+            return;
+        }
+
+        //Otherwise we have an arbitrary data structure, and we have to iterate through it.
+        List<Field> fields = getAllFields(o.getClass(), Object.class);
+        for(Field field : fields) {
+            field.setAccessible(true);
+            Object element = field.get(o);
+            if(visited.contains(element)) {
+                continue;
+            }
+            visited.add(element);
+            findValuesInObject(element, visited, foundValues, soughtClass, soughtField);
+        }
+    }
+
+    private static List<Field> getAllFields(Class<?> start, Class<?> exclusiveParent) {
+
+        List<Field> allFields = new ArrayList<>(Arrays.asList(start.getDeclaredFields()));
+
+        Class<?> parent = start.getSuperclass();
+
+        if(parent != null &&
+            (exclusiveParent == null || !(parent.equals(exclusiveParent)))
+        ) {
+            List<Field> inheritedFields = getAllFields(parent, exclusiveParent);
+            allFields.addAll(inheritedFields);
+        }
+
+        return allFields;
+    }
+
+}
